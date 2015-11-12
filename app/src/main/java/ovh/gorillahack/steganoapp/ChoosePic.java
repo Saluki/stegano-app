e9e839b7bad251449daf084293e31978cf0d6ed3
Javier <javier.lethe@gmail.com>
2015-11-12 15:39:07 +0100

    -added take picture from camera

--- app/src/main/java/ovh/gorillahack/steganoapp/ChoosePic.java
+++ app/src/main/java/ovh/gorillahack/steganoapp/ChoosePic.java
@@ -4,16 +4,22 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ChoosePic extends AppCompatActivity {
     private int PICK_IMAGE = 1;
     private int TAKE_PICTURE = 2;
+    String photoPath;
+
 
     Button takePic;
     Button choosePic;
@@ -30,15 +36,21 @@ public class ChoosePic extends AppCompatActivity {
     }
 
     public void launchCamera(View view) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, TAKE_PICTURE);
+        try {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            File file = createImageFile();
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoPath);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, TAKE_PICTURE);
+            }
+        } catch (Exception e) {
+            //TODO: gerer exception
         }
 
     }
 
     public void showGallery(View view) {
-        //lauch gallery
+        //launch gallery
         Intent intent = new Intent();
         intent.setType("image/*");//only images, no videos
         intent.setAction(Intent.ACTION_GET_CONTENT);
@@ -50,22 +62,30 @@ public class ChoosePic extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
-
             Uri uri = data.getData();
-
             try {
                 picChosen = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
 
-                //   ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                //   imageView.setImageBitmap(bitmap);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else if (requestCode == TAKE_PICTURE && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            //TODO: come back from camera
+            //do when want the gallery to know the picture is there? then we need to add code https://developer.android.com/training/camera/photobasics.html
         } else {
             //TODO:show error
         }
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(timeStamp, ".jpg", storageDir);
+
+        // Save a file: path for use with ACTION_VIEW intents
+        photoPath = "file:" + image.getAbsolutePath();
+        return image;
     }
 
 }
