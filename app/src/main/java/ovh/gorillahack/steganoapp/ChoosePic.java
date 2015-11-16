76a2c936dce445aa619020141d4f2b15fb298420
Javier <javier.lethe@gmail.com>
2015-11-16 16:15:07 +0100

    -added encoding of the pic -added confirmation pop up after encoding

--- app/src/main/java/ovh/gorillahack/steganoapp/ChoosePic.java
+++ app/src/main/java/ovh/gorillahack/steganoapp/ChoosePic.java
@@ -1,5 +1,7 @@
 package ovh.gorillahack.steganoapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -9,16 +11,21 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import ovh.gorillahack.steganoapp.algorithm.SteganoEncoder;
+
 public class ChoosePic extends AppCompatActivity {
     private int PICK_IMAGE = 1;
     private int TAKE_PICTURE = 2;
     String photoPath;
+    String messsageToEncode = "";
 
     Button takePic;
     Button choosePic;
@@ -35,17 +42,22 @@ public class ChoosePic extends AppCompatActivity {
     }
 
     public void launchCamera(View view) {
-        try {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            File file = createImageFile();
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoPath);
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                //TODO: gerer exception
+            }
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
                 startActivityForResult(takePictureIntent, TAKE_PICTURE);
             }
-        } catch (Exception e) {
-            //TODO: gerer exception
         }
-
     }
 
     public void showGallery(View view) {
@@ -76,12 +88,15 @@ public class ChoosePic extends AppCompatActivity {
             }
         } else if (requestCode == TAKE_PICTURE && resultCode == RESULT_OK && data != null && data.getData() != null) {
             //do when want the gallery to know the picture is there? then we need to add code https://developer.android.com/training/camera/photobasics.html
+            //TODO: come back from camera and put picture in picChosen
         } else {
-            //TODO:show error
+            buildTextViewPopUp(getString(R.string.error));
         }
+        buildEditTextPopUp();
+
     }
 
-    private File createImageFile() throws IOException {
+    public File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
@@ -92,4 +107,48 @@ public class ChoosePic extends AppCompatActivity {
         return image;
     }
 
+    //https://stackoverflow.com/questions/10903754/input-text-dialog-android
+    public void buildEditTextPopUp() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.messageToEnc);
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton(R.string.encode, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                messsageToEncode = input.getText().toString();
+                SteganoEncoder encoder = new SteganoEncoder(picChosen);
+                buildTextViewPopUp(getString(R.string.image_encrypt_succ));
+                try {
+                    encoder.encode(messsageToEncode);
+                } catch (Exception e) {
+                    buildTextViewPopUp(getString(R.string.error) + e.getMessage());
+                }
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    public void buildTextViewPopUp(String text) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.informations);
+
+        // Set up the input
+        TextView toShow = new TextView(this);
+        toShow.setText(text);
+        builder.setView(toShow);
+
+        builder.show();
+    }
 }
