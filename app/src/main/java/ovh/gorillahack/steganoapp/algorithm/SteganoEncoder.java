53775e361b71de6db4496cb541293f8028b30600
Saluki <saluki@gmx.fr>
2015-11-19 00:40:32 +0100

    Finishing the base draft for the 'encode' feature

--- app/src/main/java/ovh/gorillahack/steganoapp/algorithm/SteganoEncoder.java
+++ app/src/main/java/ovh/gorillahack/steganoapp/algorithm/SteganoEncoder.java
@@ -1,8 +1,11 @@
 package ovh.gorillahack.steganoapp.algorithm;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 
 import java.security.InvalidParameterException;
+import java.util.ArrayList;
 
 import ovh.gorillahack.steganoapp.exceptions.SteganoEncodeException;
 import ovh.gorillahack.steganoapp.utils.SteganoUtils;
@@ -13,11 +16,11 @@ public class SteganoEncoder {
 
     public SteganoEncoder(Bitmap pictureBitmap) {
 
-        if( pictureBitmap==null ) {
+        if (pictureBitmap == null) {
             throw new NullPointerException("Picture bitmap cannot be null");
         }
 
-        if( pictureBitmap.getWidth()==0 || pictureBitmap.getHeight()==0 ) {
+        if (pictureBitmap.getWidth() == 0 || pictureBitmap.getHeight() == 0) {
             throw new InvalidParameterException("Picture must have a valid size");
         }
 
@@ -30,17 +33,38 @@ public class SteganoEncoder {
             throw new NullPointerException("Text to encode cannot be null or empty");
         }
 
-        SteganoUtils.getBinarySequence(text);
+        int[] binarySequence = SteganoUtils.getBinarySequence(text);
+        int binaryPtr = 0;
 
+        // For each pixel in the picture
         for (int y = 0; y < this.pictureBitmap.getHeight(); y++) {
             for (int x = 0; x < this.pictureBitmap.getWidth(); x++) {
 
+                // Extract Least Significant Byte from pixel (blue component)
                 int argbColor = this.pictureBitmap.getPixel(x, y);
-                int lsb = SteganoUtils.getLastSignificantBit(argbColor);
+                int blueColor = Color.blue(argbColor);
 
-                // TODO Lsb manipulation
+                // Make the last bit 0 if needed
+                if (blueColor % 2 == 1) {
+                    blueColor--;
+                }
 
+                // There is the true encoding: injecting the data
+                blueColor += binarySequence[binaryPtr];
 
+                // Recode the color in ARGB format
+                int newArgbColor = Color.argb(Color.alpha(argbColor),
+                        Color.red(argbColor),
+                        Color.green(argbColor),
+                        blueColor);
+                this.pictureBitmap.setPixel(x, y, newArgbColor);
+
+                // Return the new bitmap when all data is encoded
+                if (binaryPtr >= binarySequence.length - 1) {
+                    return this.pictureBitmap;
+                }
+                // Move the bit pointer
+                binaryPtr++;
             }
         }
 
--- app/src/main/java/ovh/gorillahack/steganoapp/utils/SteganoUtils.java
+++ app/src/main/java/ovh/gorillahack/steganoapp/utils/SteganoUtils.java
@@ -4,15 +4,24 @@ import android.graphics.Color;
 
 public class SteganoUtils {
 
-    static public int getLastSignificantBit(int standardColor) {
-
-        int blueColor = Color.blue(standardColor);
-        int bitMask = 1;
-
-        return blueColor&bitMask;
-    }
+    public static final int UTF8_SIZE = 8;
 
     static public int[] getBinarySequence(String text) {
-        return null; // TODO
+
+        char[] textArray = text.toCharArray();
+        int[] bitList = new int[textArray.length * UTF8_SIZE];
+        int bitListPtr = 0;
+
+        for (char asciiValue : textArray) {
+
+            for (int bitWise = UTF8_SIZE - 1; bitWise >= 0; bitWise--) {
+                int tempBit = ((asciiValue & (1 << bitWise)) > 0) ? 1 : 0;
+
+                bitList[bitListPtr] = tempBit;
+                bitListPtr++;
+            }
+        }
+
+        return bitList;
     }
 }
