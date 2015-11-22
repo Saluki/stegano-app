c07cee1c7dc061ebb5fef4bb60afc139ca5af5b1
Javier <javier.lethe@gmail.com>
2015-11-22 17:40:49 +0100

    -- settings white and reset buttons added

--- app/src/main/java/ovh/gorillahack/steganoapp/domain/SettingsActivity.java
+++ app/src/main/java/ovh/gorillahack/steganoapp/domain/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -11,7 +12,7 @@ import ovh.gorillahack.steganoapp.R;
 import ovh.gorillahack.steganoapp.utils.Utils;
 
 public class SettingsActivity extends AppCompatActivity {
-    int r, b, g;
+    int r, b, g, oldR, oldB, oldG;
 
     SeekBar greenSeekBar;
     SeekBar redSeekBar;
@@ -35,18 +36,11 @@ public class SettingsActivity extends AppCompatActivity {
 
         SharedPreferences settings = getSharedPreferences(Utils.PREFS_NAME, 0);
 
-        r = settings.getInt("r", 255);
-        b = settings.getInt("b", 255);
-        g = settings.getInt("g", 255);
+        oldR = r = settings.getInt("r", 255);
+        oldB = b = settings.getInt("b", 255);
+        oldG = g = settings.getInt("g", 255);
 
-        changeBackroundColor();
-
-        redValue.setText(String.valueOf(r));
-        redSeekBar.setProgress(r);
-        greenValue.setText(String.valueOf(b));
-        greenSeekBar.setProgress(b);
-        yelowValue.setText(String.valueOf(g));
-        blueSeekBar.setProgress(g);
+        refresh();
 
         greenSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -132,6 +126,32 @@ public class SettingsActivity extends AppCompatActivity {
         e.putInt("g", g);
         e.commit();
     }
+
+    public void setToWhite(View v) {
+        oldR = r;
+        oldB = b;
+        oldG = g;
+        r = b = g = 255;
+        refresh();
+    }
+
+    public void resetToPreviousColor(View v) {
+        r = oldR;
+        b = oldB;
+        g = oldG;
+        refresh();
+    }
+
+    private void refresh() {
+        changeBackroundColor();
+        redValue.setText(String.valueOf(r));
+        redSeekBar.setProgress(r);
+        greenValue.setText(String.valueOf(b));
+        greenSeekBar.setProgress(b);
+        yelowValue.setText(String.valueOf(g));
+        blueSeekBar.setProgress(g);
+    }
+
 
     private void changeBackroundColor() {
         findViewById(R.id.activity_settings_layout).setBackgroundColor(Color.argb(255, r, b, g));
