e95c31c975f1cd33f8949c2eb64769aa5ca9dec4
Saluki <saluki@gmx.fr>
2015-11-29 03:16:34 +0100

    Finishing the locales (EN-FR)

--- app/src/main/java/ovh/gorillahack/steganoapp/domain/DecodeActivity.java
+++ app/src/main/java/ovh/gorillahack/steganoapp/domain/DecodeActivity.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RelativeLayout;
